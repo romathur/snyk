0ee45768272226ffc39ad862b684083f957e51b4
Liliana Kastilio <lili@snyk.io>
2019-10-08 13:11:56 +0100

    feat: user external spinner in npm and monitor

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -10,7 +10,7 @@ import * as alerts from '../lib/alerts';
 import * as sln from '../lib/sln';
 import { args as argsLib, Args } from './args';
 import { copy } from './copy';
-import spinner = require('../lib/spinner');
+
 import errors = require('../lib/errors/legacy-errors');
 import ansiEscapes = require('ansi-escapes');
 import { isPathToPackageFile } from '../lib/detect';
@@ -43,7 +43,6 @@ async function runCommand(args: Args) {
 }
 
 async function handleError(args, error) {
-  spinner.clearAll();
   let command = 'bad-command';
   let exitCode = EXIT_CODES.ERROR;
 
